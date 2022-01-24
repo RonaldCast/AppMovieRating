aa50a9aa23aaa91bbfb6fd0c3a3c7a297d901c62
Ronald Castillo <ronaldcastillo789@gmail.com>
2022-01-23 21:19:57 -0400

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -3,8 +3,8 @@
 module.exports = {
     DB: 'mongodb://localhost/movie_rating_app',
     SECRETE: 'movieratingappsecretkey', 
-    FACEBOOK_APP_ID: '571266206698906',
-    FACEBOOK_APP_SECRET: '27a974383861d8cab3cee694b43ac51a',
-    GOOGLE_APP_ID: '141842000962-a6e2is6iufdsaid7frvf47uujmbtpvva.apps.googleusercontent.com',
-    GOOGLE_APP_SECRET: '8VDmQzHhd4mlYcGuiv-qzJwv'
+    FACEBOOK_APP_ID: '',
+    FACEBOOK_APP_SECRET: '',
+    GOOGLE_APP_ID: '',
+    GOOGLE_APP_SECRET: ''
 }
